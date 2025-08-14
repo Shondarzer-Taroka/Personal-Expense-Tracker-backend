c222aae1286b890100fe4cca5163b044213e39f3
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-08-14 21:38:58 +0600

    user model created

---
+++ src/models/user.model.js
@@ -0,0 +1,33 @@
+// src/models/user.model.js
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      minlength: [2, "Name must be at least 2 characters long"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/.+\@.+\..+/, "Please enter a valid email address"],
+    },
+    photo: {
+      type: String,
+      required: [true, "Profile photo URL is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+// Create a compound index for faster email lookups
+userSchema.index({ email: 1 });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
