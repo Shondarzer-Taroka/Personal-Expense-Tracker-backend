ac45828d8a28a5db4a9a6cec58cbe0be8353f9ac
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-08-14 22:46:29 +0600

    jsonwebtoken installed

---
+++ src/middlewares/auth.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+export const protect = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) return res.status(401).json({ success: false, message: "Not authorized" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "your_jwt_secret");
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ success: false, message: "Token invalid" });
+  }
+};
