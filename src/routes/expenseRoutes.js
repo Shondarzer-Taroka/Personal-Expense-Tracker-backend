1bb4cb6b019eb448662f147bc9ee5b5820ed9860
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-08-14 19:49:55 +0600

    routes created

---
+++ src/routes/expenseRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import {
+  getExpenses,
+  addExpense,
+  updateExpense,
+  deleteExpense
+} from '../controllers/expenseController.js';
+
+const router = express.Router();
+
+router.route('/')
+  .get(getExpenses)
+  .post(addExpense);
+
+router.route('/:id')
+  .patch(updateExpense)
+  .delete(deleteExpense);
+
+export default router;
