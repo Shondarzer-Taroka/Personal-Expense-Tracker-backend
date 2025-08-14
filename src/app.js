cd3a239581c19229bb078a3c359e6e7bab786d0a
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-08-14 20:06:18 +0600

    connection database error and es module erorr solved

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import expenseRoutes from './routes/expenseRoutes.js';
+import { errorHandler, notFound } from './utils/errorHandler.js';
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/expenses', expenseRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+export default app;
